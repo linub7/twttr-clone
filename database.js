e116f5cdc1bc709cb8715e6f76bac8ba480c0589
lin7 <linub7@gmail.com>
2021-06-30 11:38:29 +0430

    Ready for Heroku

--- database.js
+++ database.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useUnifiedTopology", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useUnifiedTopology", true);
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
 
 class Database {
-    constructor() {
-        this.connect();
-    }
+  constructor() {
+    this.connect();
+  }
 
-    connect() {
-        mongoose
-            .connect(
-                "mongodb+srv://admin:dbuserpassword@twitterclonecluster.by2jq.mongodb.net/TwitterCloneDB?retryWrites=true&w=majority"
-            )
-            .then(() => {
-                console.log("DATABASE connection Successful");
-            })
-            .catch((err) => {
-                console.log(`DATABASE connection error: ${err}`);
-            });
-    }
+  connect() {
+    mongoose
+      .connect(
+        'mongodb+srv://admin:TFRcNdghUHC1KZVU@twitterclonecluster.by2jq.mongodb.net/TwitterCloneDB?retryWrites=true&w=majority'
+      )
+      .then(() => {
+        console.log('DATABASE connection Successful');
+      })
+      .catch((err) => {
+        console.log(`DATABASE connection error: ${err}`);
+      });
+  }
 }
 
 module.exports = new Database();
